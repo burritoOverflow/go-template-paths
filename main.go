56897bbf60defdb324bcf3c712907d7791fe5027
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2025-05-15 23:29:30 -0400

    Refactor mux implementation to take arbitrary routes and params.

--- main.go
+++ main.go
@@ -10,7 +10,13 @@ import (
 	"strings"
 )
 
+// simple hardcoded implementation
 func dynamicPathHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	// Define the pattern we're looking for
 	pathPattern := regexp.MustCompile(`^/foo/bar/([a-zA-Z0-9]+)/baz/([a-zA-Z0-9]+)/qux$`)
 	matches := pathPattern.FindStringSubmatch(r.URL.Path)
@@ -30,6 +36,56 @@ func dynamicPathHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Path parameters received:\n")
 	fmt.Fprintf(w, "First parameter: %s\n", param1)
 	fmt.Fprintf(w, "Second parameter: %s\n", param2)
+}
+
+// Convert a provided pattern path pattern from i.e "/foo/bar/%s/baz/%s/qux" to a proper alphanumeric regex
+func makeRegexPatternStr(pattern string) string {
+	return "^" + strings.Replace(pattern, "%s", "([a-zA-Z0-9]+)", -1) + "$"
+}
+
+// creates an http.HandlerFunc that matches the request path against the provided templated path and extracts parameters
+// i.e provide "/foo/bar/%s/baz/%s/qux" and it will match paths like "/foo/bar/123/baz/456/qux"
+func newPathRegexHandler(routeTemplateStr string) http.HandlerFunc {
+	// internally generate the regex pattern from the template
+	regexPatternStr := makeRegexPatternStr(routeTemplateStr)
+	pathPattern := regexp.MustCompile(regexPatternStr)
+	numGroups := pathPattern.NumSubexp()
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		matches := pathPattern.FindStringSubmatch(r.URL.Path)
+		// The handler must ensure the *full* path matches the specific regex.
+		if matches == nil {
+			log.Printf("No matches for pattern '%s' in path '%s'", regexPatternStr, r.URL.Path)
+			http.NotFound(w, r)
+			return
+		}
+
+		// matches[0] is the full string, we're only interested in the capturing groups
+		if len(matches)-1 != numGroups {
+			log.Printf("Error: Expected %d capturing groups, got %d from path '%s' with pattern '%s'",
+				numGroups, len(matches)-1, r.URL.Path, regexPatternStr)
+
+			http.Error(w, "Internal server error: Mismatched capturing groups", http.StatusInternalServerError)
+			return
+		}
+
+		log.Printf("%d path parameters captured from path '%s' using pattern '%s': %v (full match: %s)\n",
+			numGroups, r.URL.Path, regexPatternStr, matches[1:], matches[0])
+
+		if numGroups > 0 {
+			for i := 0; i < numGroups; i++ {
+				// we'll return each parameter in the response
+				fmt.Fprintf(w, "Parameter %d: %s\n", i+1, matches[i+1])
+			}
+		} else {
+			fmt.Fprintln(w, "No parameters captured.")
+		}
+	}
 }
 
 // associates a pattern with a handler
@@ -57,6 +113,11 @@ func (r *customRouter) HandleFunc(pattern string, handler http.HandlerFunc) {
 type paramKey int
 
 func (r *customRouter) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	for _, route := range r.routes {
 		matches := route.pattern.FindStringSubmatch(req.URL.Path)
 		if matches != nil {
@@ -87,7 +148,13 @@ func getParam(r *http.Request, index int) string {
 }
 
 // Handler function for the custom router
+// TODO extend in the same manner as the mux implementation
 func dynamicPathHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	param1 := getParam(r, 1)
 	param2 := getParam(r, 2)
 
@@ -113,11 +180,45 @@ func main() {
 		log.Printf("Starting server with custom router on %s...", addr)
 		log.Fatal(http.ListenAndServe(addr, router))
 	} else {
-		// Method 2: Using http.ServeMux with regexp in the handler
+		// Method 2: Using http.ServeMux with a generalized regex handler
 		mux := http.NewServeMux()
-		// Match the prefix and parse inside
-		mux.HandleFunc("/foo/bar/", dynamicPathHandler)
+
+		// Define the path template(s)
+		routeTemplateStr := "/foo/bar/%s/baz/%s/qux"
+		apiRouteTemplStr := "/api/v3/%s/%s"
+		plainRouteTemplStr := "/blah/foo/bar/baz/qux"
+
+		// and create and register handlers
+		registerHandlerForPath(mux, routeTemplateStr)
+		registerHandlerForPath(mux, apiRouteTemplStr)
+		registerHandlerForPath(mux, plainRouteTemplStr)
+
 		log.Printf("Starting server with ServeMux on %s", addr)
+		log.Printf("ServeMux handles prefix '%s'", routeTemplateStr)
 		log.Fatal(http.ListenAndServe(addr, mux))
 	}
+}
+
+// registerHandlerForPath registers a handler for the given path template with the provided ServeMux
+func registerHandlerForPath(mux *http.ServeMux, routeTemplateStr string) {
+	// here, convert a path like "/foo/bar/%s/baz/%s/qux" to "/foo/bar/" to register just the 'prefix'
+	// (prior to the template positions)
+	pathPrefixForMux := getPathPrefix(routeTemplateStr)
+	log.Printf("Registering handler for path prefix: '%s' with template %s\n", pathPrefixForMux, routeTemplateStr)
+
+	// create and register the handler for the given path
+	handler := newPathRegexHandler(routeTemplateStr)
+	mux.HandleFunc(pathPrefixForMux, handler)
+}
+
+// return the prefix of the path pattern up to the first '%s' occurrence
+func getPathPrefix(pattern string) string {
+	// Find the first occurrence of '%s' and return the prefix up to that point
+	// i.e "/foo/bar/%s/baz/%s/qux" -> "/foo/bar/"
+	if idx := strings.Index(pattern, "%s"); idx != -1 {
+		return pattern[:idx]
+	}
+	// allow for non-template routes as well
+	// If no '%s' found, return the whole pattern
+	return pattern
 }
--- main_test.go
+++ main_test.go
@@ -91,6 +91,241 @@ func TestDynamicPathHandler(t *testing.T) {
 	}
 }
 
+func TestNewPathRegexHandler(t *testing.T) {
+	routeTemplateStr := "/foo/bar/%s/baz/%s/qux"
+
+	tests := []struct {
+		name           string
+		path           string
+		expectedStatus int
+		expectedBody   string
+	}{
+		{
+			name:           "valid path",
+			path:           "/foo/bar/alpha123/baz/beta456/qux",
+			expectedStatus: http.StatusOK,
+			expectedBody:   "Parameter 1: alpha123\nParameter 2: beta456\n",
+		},
+		{
+			name:           "invalid path - too few segments",
+			path:           "/foo/bar/alpha123/baz/qux",
+			expectedStatus: http.StatusNotFound,
+			expectedBody:   "404 page not found\n",
+		},
+		{
+			name:           "invalid path - non-alphanumeric param1",
+			path:           "/foo/bar/alpha-123/baz/beta456/qux",
+			expectedStatus: http.StatusNotFound,
+			expectedBody:   "404 page not found\n",
+		},
+		{
+			name:           "invalid path - non-alphanumeric param2",
+			path:           "/foo/bar/alpha123/baz/beta_456/qux",
+			expectedStatus: http.StatusNotFound,
+			expectedBody:   "404 page not found\n",
+		},
+		{
+			name:           "path prefix matches but full path does not",
+			path:           "/foo/bar/not/enough/segments",
+			expectedStatus: http.StatusNotFound,
+			expectedBody:   "404 page not found\n",
+		},
+		{
+			name:           "invalid path - `baz` segment missing",
+			path:           "/foo/bar/paramOne/paramTwo/qux",
+			expectedStatus: http.StatusNotFound,
+			expectedBody:   "404 page not found\n",
+		},
+		{
+			name:           "invalid path - `qux` segment missing",
+			path:           "/foo/bar/paramOne/baz/paramTwo",
+			expectedStatus: http.StatusNotFound,
+			expectedBody:   "404 page not found\n",
+		},
+		{
+			name:           "invalid path - format correct with different segments",
+			path:           "/foo/bar/paramOne/baaz/paramTwo/quux",
+			expectedStatus: http.StatusNotFound,
+			expectedBody:   "404 page not found\n",
+		},
+	}
+
+	handler := newPathRegexHandler(routeTemplateStr)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req, err := http.NewRequest("GET", tt.path, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rr := httptest.NewRecorder()
+			handler.ServeHTTP(rr, req)
+
+			if status := rr.Code; status != tt.expectedStatus {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					status, tt.expectedStatus)
+			}
+
+			if strings.TrimSpace(rr.Body.String()) != strings.TrimSpace(tt.expectedBody) {
+				t.Errorf("handler returned unexpected body: got %v want %v",
+					rr.Body.String(), tt.expectedBody)
+			}
+		})
+	}
+
+	t.Run("no parameters in route", func(t *testing.T) {
+		noParamRoute := "/static/path"
+		noParamHandler := newPathRegexHandler(noParamRoute)
+		expectedNoParamBody := "No parameters captured.\n"
+
+		req, err := http.NewRequest("GET", noParamRoute, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		rr := httptest.NewRecorder()
+		noParamHandler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+		}
+
+		if strings.TrimSpace(rr.Body.String()) != strings.TrimSpace(expectedNoParamBody) {
+			t.Errorf("handler returned unexpected body: got %q want %q", rr.Body.String(), expectedNoParamBody)
+		}
+
+		// Test mismatch for no param route
+		reqMismatch, errMismatch := http.NewRequest("GET", "/static/other", nil)
+		if errMismatch != nil {
+			t.Fatal(errMismatch)
+		}
+
+		rrMismatch := httptest.NewRecorder()
+		noParamHandler.ServeHTTP(rrMismatch, reqMismatch)
+		if status := rrMismatch.Code; status != http.StatusNotFound {
+			t.Errorf("handler returned wrong status code for mismatch: got %v want %v", status, http.StatusNotFound)
+		}
+	})
+
+	t.Run("one parameter in route", func(t *testing.T) {
+		oneParamRoute := "/user/%s/profile"
+		oneParamHandler := newPathRegexHandler(oneParamRoute)
+		expectedOneParamBody := "Parameter 1: testuser\n"
+
+		req, err := http.NewRequest("GET", "/user/testuser/profile", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		rr := httptest.NewRecorder()
+		oneParamHandler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+		}
+
+		if strings.TrimSpace(rr.Body.String()) != strings.TrimSpace(expectedOneParamBody) {
+			t.Errorf("handler returned unexpected body: got %q want %q", rr.Body.String(), expectedOneParamBody)
+		}
+	})
+
+	t.Run("two parameters in route", func(t *testing.T) {
+		multiParamRoute := "/foo/%s/bar/%s/baz"
+		multiParamHandler := newPathRegexHandler(multiParamRoute)
+		expectedMultiParamBody := "Parameter 1: param1\nParameter 2: param2\n"
+
+		req, err := http.NewRequest("GET", "/foo/param1/bar/param2/baz", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		rr := httptest.NewRecorder()
+		multiParamHandler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+		}
+		if strings.TrimSpace(rr.Body.String()) != strings.TrimSpace(expectedMultiParamBody) {
+			t.Errorf("handler returned unexpected body: got %q want %q", rr.Body.String(), expectedMultiParamBody)
+		}
+	})
+
+	t.Run("three parameters in route", func(t *testing.T) {
+		threeParamRoute := "/api/v3/%s/%s/%s"
+		threeParamHandler := newPathRegexHandler(threeParamRoute)
+		expectedThreeParamBody := "Parameter 1: param1\nParameter 2: param2\nParameter 3: param3\n"
+
+		req, err := http.NewRequest("GET", "/api/v3/param1/param2/param3", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		rr := httptest.NewRecorder()
+		threeParamHandler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+		}
+		if strings.TrimSpace(rr.Body.String()) != strings.TrimSpace(expectedThreeParamBody) {
+			t.Errorf("handler returned unexpected body: got %q want %q", rr.Body.String(), expectedThreeParamBody)
+		}
+	})
+
+	t.Run("three parameters in route with other segments", func(t *testing.T) {
+		threeParamRoute := "/api/v3/%s/other/%s/more/%s"
+		threeParamHandler := newPathRegexHandler(threeParamRoute)
+		expectedThreeParamBody := "Parameter 1: param1\nParameter 2: param2\nParameter 3: param3\n"
+
+		req, err := http.NewRequest("GET", "/api/v3/param1/other/param2/more/param3", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		rr := httptest.NewRecorder()
+		threeParamHandler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+		}
+		if strings.TrimSpace(rr.Body.String()) != strings.TrimSpace(expectedThreeParamBody) {
+			t.Errorf("handler returned unexpected body: got %q want %q", rr.Body.String(), expectedThreeParamBody)
+		}
+	})
+
+	t.Run("invalid path for two parameters", func(t *testing.T) {
+		multiParamRoute := "/foo/%s/bar/%s/baz"
+		multiParamHandler := newPathRegexHandler(multiParamRoute)
+
+		req, err := http.NewRequest("GET", "/foo/param1/bar/param2/invalid", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		rr := httptest.NewRecorder()
+		multiParamHandler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusNotFound {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusNotFound)
+		}
+	})
+
+	t.Run("invalid method", func(t *testing.T) {
+		multiParamRoute := "/foo/%s/bar/%s/baz"
+		multiParamHandler := newPathRegexHandler(multiParamRoute)
+
+		req, err := http.NewRequest("POST", "/foo/param1/bar/param2/baz", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		rr := httptest.NewRecorder()
+		multiParamHandler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusMethodNotAllowed {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusMethodNotAllowed)
+		}
+
+		expectedBody := "Method not allowed\n"
+		if strings.TrimSpace(rr.Body.String()) != strings.TrimSpace(expectedBody) {
+			t.Errorf("handler returned unexpected body: got %q want %q", rr.Body.String(), expectedBody)
+		}
+	})
+}
+
 func TestCustomRouter(t *testing.T) {
 	router := &customRouter{}
 	router.HandleFunc("/foo/bar/%s/baz/%s/qux", dynamicPathHandlerFunc)
@@ -175,6 +410,117 @@ func TestCustomRouter(t *testing.T) {
 			if strings.TrimSpace(rr.Body.String()) != strings.TrimSpace(tt.expectedBody) {
 				t.Errorf("handler returned unexpected body: got %v want %v",
 					rr.Body.String(), tt.expectedBody)
+			}
+		})
+	}
+}
+
+func TestMakeRegexPatternStr(t *testing.T) {
+	tests := []struct {
+		name     string
+		pattern  string
+		expected string
+	}{
+		{
+			name:     "single %s",
+			pattern:  "/api/users/%s/profile",
+			expected: "^/api/users/([a-zA-Z0-9]+)/profile$",
+		},
+		{
+			name:     "multiple %s",
+			pattern:  "/foo/%s/bar/%s/baz",
+			expected: "^/foo/([a-zA-Z0-9]+)/bar/([a-zA-Z0-9]+)/baz$",
+		},
+		{
+			name:     "no %s",
+			pattern:  "/static/path",
+			expected: "^/static/path$",
+		},
+		{
+			name:     "%s at the beginning",
+			pattern:  "%s/path/end",
+			expected: "^([a-zA-Z0-9]+)/path/end$",
+		},
+		{
+			name:     "%s at the end",
+			pattern:  "/path/start/%s",
+			expected: "^/path/start/([a-zA-Z0-9]+)$",
+		},
+		{
+			name:     "empty pattern",
+			pattern:  "",
+			expected: "^$",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := makeRegexPatternStr(tt.pattern)
+			if result != tt.expected {
+				t.Errorf("makeRegexPatternStr(%q) = %q; want %q", tt.pattern, result, tt.expected)
+			}
+		})
+	}
+}
+
+func TestGetPathPrefix(t *testing.T) {
+	tests := []struct {
+		name     string
+		pattern  string
+		expected string
+	}{
+		{
+			name:     "multiple %s",
+			pattern:  "/foo/bar/%s/baz/%s/qux",
+			expected: "/foo/bar/",
+		},
+		{
+			name:     "single %s",
+			pattern:  "/api/users/%s/profile",
+			expected: "/api/users/",
+		},
+		{
+			name:     "multiple %s",
+			pattern:  "/foo/%s/bar/%s/baz",
+			expected: "/foo/",
+		},
+		{
+			name:     "no %s",
+			pattern:  "/static/path",
+			expected: "/static/path",
+		},
+		{
+			name:     "%s at the beginning",
+			pattern:  "%s/path/end",
+			expected: "",
+		},
+		{
+			name:     "%s at the end with preceding slash",
+			pattern:  "/path/start/%s",
+			expected: "/path/start/",
+		},
+		{
+			name:     "empty pattern",
+			pattern:  "",
+			expected: "",
+		},
+		{
+			name:     "pattern is just %s",
+			pattern:  "%s",
+			expected: "",
+		},
+		{
+			name:     "pattern is just /%s",
+			pattern:  "/%s",
+			expected: "/",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := getPathPrefix(tt.pattern)
+			if result != tt.expected {
+				t.Errorf("getPathPrefix(%q) = %q; want %q", tt.pattern, result, tt.expected)
 			}
 		})
 	}
